dbc44e83d3f2bed13488fc5f5f7b8b512f4fdf2b
Zhi Huang <zhi.huang@compass.com>
2022-05-19 16:16:02 +0800

    update: development

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./index.css";
+import { a } from "./a";
+import { b } from "./b";
+import { c } from "./c";
+import { d } from "./d";
+import { e } from "./e";
+import { f } from "./f";
+import { g } from "./g";
+
+console.log(a, b, c, d, e, f, g);
