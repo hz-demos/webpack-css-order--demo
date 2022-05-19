43c24a6eb3b7068dc9f34db2ec8c9f974cfe737e
Zhi Huang <zhi.huang@compass.com>
2022-05-19 15:54:58 +0800

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+import { a } from "./src/a";
+import { b } from "./src/b";
+import { c } from "./src/c";
+import { d } from "./src/d";
+import { e } from "./src/e";
+import { f } from "./src/f";
+import { g } from "./src/g";
